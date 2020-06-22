928a1a564af84e8f075605653f7095be5806d162
Ziye <504250110@qq.com>
2020-06-22 21:06:14 +0800

    Update kernels.c

--- Lab3/perflab/kernels.c
+++ Lab3/perflab/kernels.c
@@ -176,13 +176,195 @@ void naive_smooth(int dim, pixel *src, pixel *dst)
 char smooth_descr[] = "smooth: Current working version";
 void smooth(int dim, pixel *src, pixel *dst) 
 {
-    int i, j;
+    int i, j, pos;
+    pixel sub_sum[3];
+    pixel row_sum[3];
 
-    for (i = 0; i < dim; i++)
+    // left_top
+    dst[0].red = (src[0].red + src[1].red + src[dim].red + src[dim+1].red) >> 2;
+    dst[0].green = (src[0].green + src[1].green + src[dim].green + src[dim+1].green) >> 2;
+    dst[0].blue = (src[0].blue + src[1].blue + src[dim].blue + src[dim+1].blue) >> 2;
+
+    // right_top
+    dst[dim-1].red = (src[dim-2].red + src[dim-1].red + src[dim*2-2].red + src[dim*2-1].red) >> 2;
+    dst[dim-1].green = (src[dim-2].green + src[dim-1].green + src[dim*2-2].green + src[dim*2-1].green) >> 2;
+    dst[dim-1].blue = (src[dim-2].blue + src[dim-1].blue + src[dim*2-2].blue + src[dim*2-1].blue) >> 2;
+
+    // left_bottom
+    dst[dim*dim-dim].red = (src[dim*dim-dim-dim].red + src[dim*dim-dim-dim+1].red + src[dim*dim-dim].red + src[dim*dim-dim+1].red) >> 2;
+    dst[dim*dim-dim].green = (src[dim*dim-dim-dim].green + src[dim*dim-dim-dim+1].green + src[dim*dim-dim].green + src[dim*dim-dim+1].green) >> 2;
+    dst[dim*dim-dim].blue = (src[dim*dim-dim-dim].blue + src[dim*dim-dim-dim+1].blue + src[dim*dim-dim].blue + src[dim*dim-dim+1].blue) >> 2;
+
+    // right_bottom
+    dst[dim*dim-1].red = (src[dim*dim-dim-2].red + src[dim*dim-dim-1].red + src[dim*dim-2].red + src[dim*dim-1].red) >> 2;
+    dst[dim*dim-1].green = (src[dim*dim-dim-2].green + src[dim*dim-dim-1].green + src[dim*dim-2].green + src[dim*dim-1].green) >> 2;
+    dst[dim*dim-1].blue = (src[dim*dim-dim-2].blue + src[dim*dim-dim-1].blue + src[dim*dim-2].blue + src[dim*dim-1].blue) >> 2;
+
+    // top
+    sub_sum[0].red = (src[0].red + src[dim].red);
+    sub_sum[0].green = (src[0].green + src[dim].green);
+    sub_sum[0].blue = (src[0].blue + src[dim].blue);
+
+    sub_sum[1].red = (src[1].red + src[dim+1].red);
+    sub_sum[1].green = (src[1].green + src[dim+1].green);
+    sub_sum[1].blue = (src[1].blue + src[dim+1].blue);
+
+    sub_sum[2].red = (src[2].red + src[dim+2].red);
+    sub_sum[2].green = (src[2].green + src[dim+2].green);
+    sub_sum[2].blue = (src[2].blue + src[dim+2].blue);
+    
+    dst[1].red = (sub_sum[0].red+sub_sum[1].red+sub_sum[2].red) / 6;
+    dst[1].green = (sub_sum[0].green+sub_sum[1].green+sub_sum[2].green) / 6;
+    dst[1].blue = (sub_sum[0].blue+sub_sum[1].blue+sub_sum[2].blue) / 6;
+
+    for(i=2; i<dim-1; ++i)
     {
-        for (j = 0; j < dim; j++)
+        sub_sum[0]=sub_sum[1];
+        sub_sum[1]=sub_sum[2];
+
+        sub_sum[2].red = (src[i].red + src[dim+i].red);
+        sub_sum[2].green = (src[i].green + src[dim+i].green);
+        sub_sum[2].blue = (src[i].blue + src[dim+i].blue);
+
+        pos = i-1;
+
+        dst[pos].red = (sub_sum[0].red+sub_sum[1].red+sub_sum[2].red) / 6;
+        dst[pos].green = (sub_sum[0].green+sub_sum[1].green+sub_sum[2].green) / 6;
+        dst[pos].blue = (sub_sum[0].blue+sub_sum[1].blue+sub_sum[2].blue) / 6;
+    }
+
+    // bottom
+    sub_sum[0].red = (src[dim*dim-dim-dim].red + src[dim*dim-dim].red);
+    sub_sum[0].green = (src[dim*dim-dim-dim].green + src[dim*dim-dim].green);
+    sub_sum[0].blue = (src[dim*dim-dim-dim].blue + src[dim*dim-dim].blue);
+
+    sub_sum[1].red = (src[dim*dim-dim-dim+1].red + src[dim*dim-dim+1].red);
+    sub_sum[1].green = (src[dim*dim-dim-dim+1].green + src[dim*dim-dim+1].green);
+    sub_sum[1].blue = (src[dim*dim-dim-dim+1].blue + src[dim*dim-dim+1].blue);
+
+    sub_sum[2].red = (src[dim*dim-dim-dim+2].red + src[dim*dim-dim+2].red);
+    sub_sum[2].green = (src[dim*dim-dim-dim+2].green + src[dim*dim-dim+2].green);
+    sub_sum[2].blue = (src[dim*dim-dim-dim+2].blue + src[dim*dim-dim+2].blue);
+    
+    dst[1].red = (sub_sum[0].red+sub_sum[1].red+sub_sum[2].red) / 6;
+    dst[1].green = (sub_sum[0].green+sub_sum[1].green+sub_sum[2].green) / 6;
+    dst[1].blue = (sub_sum[0].blue+sub_sum[1].blue+sub_sum[2].blue) / 6;
+
+    for(i=dim*dim-dim+2; i<dim*dim-1; ++i)
+    {
+        sub_sum[0]=sub_sum[1];
+        sub_sum[1]=sub_sum[2];
+
+        sub_sum[2].red = (src[i-dim].red + src[i].red);
+        sub_sum[2].green = (src[i-dim].green + src[i].green);
+        sub_sum[2].blue = (src[i-dim].blue + src[i].blue);
+
+        pos = i-1;
+
+        dst[pos].red = (sub_sum[0].red+sub_sum[1].red+sub_sum[2].red) / 6;
+        dst[pos].green = (sub_sum[0].green+sub_sum[1].green+sub_sum[2].green) / 6;
+        dst[pos].blue = (sub_sum[0].blue+sub_sum[1].blue+sub_sum[2].blue) / 6;
+    }
+
+    // left
+    sub_sum[0].red = (src[0].red + src[1].red);
+    sub_sum[0].green = (src[0].green + src[1].green);
+    sub_sum[0].blue = (src[0].blue + src[1].blue);
+
+    sub_sum[1].red = (src[dim].red + src[dim+1].red);
+    sub_sum[1].green = (src[dim].green + src[dim+1].green);
+    sub_sum[1].blue = (src[dim].blue + src[dim+1].blue);
+
+    sub_sum[2].red = (src[dim+dim].red + src[dim+dim+1].red);
+    sub_sum[2].green = (src[dim+dim].green + src[dim+dim+1].green);
+    sub_sum[2].blue = (src[dim+dim].blue + src[dim+dim+1].blue);
+    
+    dst[1].red = (sub_sum[0].red+sub_sum[1].red+sub_sum[2].red) / 6;
+    dst[1].green = (sub_sum[0].green+sub_sum[1].green+sub_sum[2].green) / 6;
+    dst[1].blue = (sub_sum[0].blue+sub_sum[1].blue+sub_sum[2].blue) / 6;
+
+    for(i=dim*3; i<dim*dim-1; i+=dim)
+    {
+        sub_sum[0]=sub_sum[1];
+        sub_sum[1]=sub_sum[2];
+
+        sub_sum[2].red = (src[i].red + src[i+1].red);
+        sub_sum[2].green = (src[i].green + src[i+1].green);
+        sub_sum[2].blue = (src[i].blue + src[i+1].blue);
+
+        pos = i - dim;
+
+        dst[pos].red = (sub_sum[0].red+sub_sum[1].red+sub_sum[2].red) / 6;
+        dst[pos].green = (sub_sum[0].green+sub_sum[1].green+sub_sum[2].green) / 6;
+        dst[pos].blue = (sub_sum[0].blue+sub_sum[1].blue+sub_sum[2].blue) / 6;
+    }
+
+    // right
+    sub_sum[0].red = (src[dim-2].red + src[dim-1].red);
+    sub_sum[0].green = (src[dim-2].green + src[dim-1].green);
+    sub_sum[0].blue = (src[dim-2].blue + src[dim-1].blue);
+
+    sub_sum[1].red = (src[dim+dim-2].red + src[dim+dim-1].red);
+    sub_sum[1].green = (src[dim+dim-2].green + src[dim+dim-1].green);
+    sub_sum[1].blue = (src[dim+dim-2].blue + src[dim+dim-1].blue);
+
+    sub_sum[2].red = (src[dim*3-2].red + src[dim*3-1].red);
+    sub_sum[2].green = (src[dim*3-2].green + src[dim*3-1].green);
+    sub_sum[2].blue = (src[dim*3-2].blue + src[dim*3-1].blue);
+    
+    dst[1].red = (sub_sum[0].red+sub_sum[1].red+sub_sum[2].red) / 6;
+    dst[1].green = (sub_sum[0].green+sub_sum[1].green+sub_sum[2].green) / 6;
+    dst[1].blue = (sub_sum[0].blue+sub_sum[1].blue+sub_sum[2].blue) / 6;
+
+    for(i=dim*4-1; i<dim*dim-1; i+=dim)
+    {
+        sub_sum[0]=sub_sum[1];
+        sub_sum[1]=sub_sum[2];
+
+        sub_sum[2].red = (src[i-1].red + src[i].red);
+        sub_sum[2].green = (src[i-1].green + src[i].green);
+        sub_sum[2].blue = (src[i-1].blue + src[i].blue);
+
+        pos = i - dim;
+
+        dst[pos].red = (sub_sum[0].red+sub_sum[1].red+sub_sum[2].red) / 6;
+        dst[pos].green = (sub_sum[0].green+sub_sum[1].green+sub_sum[2].green) / 6;
+        dst[pos].blue = (sub_sum[0].blue+sub_sum[1].blue+sub_sum[2].blue) / 6;
+    }
+
+    // other
+    for(i=1; i<dim-1; i++)
+    {
+        j = 1;
+        sub_sum[0].red = (src[(i-1)*dim].red+src[i*dim].red+src[(i+1)*dim].red);
+        sub_sum[0].green = (src[(i-1)*dim].green+src[i*dim].green+src[(i+1)*dim].green);
+        sub_sum[0].blue = (src[(i-1)*dim].blue+src[i*dim].blue+src[(i+1)*dim].blue);
+
+        sub_sum[1].red = (src[(i-1)*dim+1].red+src[i*dim+1].red+src[(i+1)*dim+1].red);
+        sub_sum[1].green = (src[(i-1)*dim+1].green+src[i*dim+1].green+src[(i+1)*dim+1].green);
+        sub_sum[1].blue = (src[(i-1)*dim+1].blue+src[i*dim+1].blue+src[(i+1)*dim+1].blue);
+
+        sub_sum[2].red = (src[(i-1)*dim+2].red+src[i*dim+2].red+src[(i+1)*dim+2].red);
+        sub_sum[2].green = (src[(i-1)*dim+2].green+src[i*dim+2].green+src[(i+1)*dim+2].green);
+        sub_sum[2].blue = (src[(i-1)*dim+2].blue+src[i*dim+2].blue+src[(i+1)*dim+2].blue);
+
+        dst[i*dim+1].red = (sub_sum[0].red+sub_sum[1].red+sub_sum[2].red) / 9;
+        dst[i*dim+1].green = (sub_sum[0].green+sub_sum[1].green+sub_sum[2].green) / 9;
+        dst[i*dim+1].blue = (sub_sum[0].blue+sub_sum[1].blue+sub_sum[2].blue) / 9;
+
+        for(j=2; j<dim-1; ++j)
         {
-            dst[RIDX(i, j, dim)] = avg(dim, i, j, src);
+            sub_sum[0]=sub_sum[1];
+            sub_sum[1]=sub_sum[2];
+
+            sub_sum[2].red = (src[(i-1)*dim+j].red+src[i*dim+j].red+src[(i+1)*dim+j].red);
+            sub_sum[2].red = (src[(i-1)*dim+j].red+src[i*dim+j].red+src[(i+1)*dim+j].red);
+            sub_sum[2].red = (src[(i-1)*dim+j].red+src[i*dim+j].red+src[(i+1)*dim+j].red);
+
+            pos = j-1;
+            dst[i*dim+pos].red = (sub_sum[0].red+sub_sum[1].red+sub_sum[2].red) / 9;
+            dst[i*dim+pos].green = (sub_sum[0].green+sub_sum[1].green+sub_sum[2].green) / 9;
+            dst[i*dim+pos].blue = (sub_sum[0].blue+sub_sum[1].blue+sub_sum[2].blue) / 9;
         }
     }
 }
