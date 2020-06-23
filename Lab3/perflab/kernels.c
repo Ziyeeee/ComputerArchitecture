0288789ab9f3b08dd4fa1407ca41647820514bb9
Ziye <504250110@qq.com>
2020-06-23 17:45:26 +0800

    Update kernels.c

--- Lab3/perflab/kernels.c
+++ Lab3/perflab/kernels.c
@@ -40,11 +40,11 @@ void naive_rotate(int dim, pixel *src, pixel *dst)
  * IMPORTANT: This is the version you will be graded on
  */
 char rotate_descr[] = "rotate: Current working version";
-void rotate(int dim, pixel *src, pixel *dst) 
+void rotate_16(int dim, pixel *src, pixel *dst) 
 {
     int i, j;
     int dstPos, srcPos;
-    int block=32;//blocking the Matrix
+    int block=16;//blocking the Matrix
     for(i=0; i<dim; i+=block)
     {
         for(j=0; j<dim; ++j)
@@ -54,6 +54,64 @@ void rotate(int dim, pixel *src, pixel *dst)
             dst[dstPos] = src[srcPos];
             ++dstPos;
             srcPos += dim;
+            dst[dstPos] = src[srcPos];
+            ++dstPos;
+            srcPos += dim;
+            dst[dstPos] = src[srcPos];
+            ++dstPos;
+            srcPos += dim;
+            dst[dstPos] = src[srcPos];
+            ++dstPos;
+            srcPos += dim;
+            dst[dstPos] = src[srcPos];
+            ++dstPos;
+            srcPos += dim;
+            dst[dstPos] = src[srcPos];
+            ++dstPos;
+            srcPos += dim;
+            dst[dstPos] = src[srcPos];
+            ++dstPos;
+            srcPos += dim;
+            dst[dstPos] = src[srcPos];
+            ++dstPos;
+            srcPos += dim;
+            dst[dstPos] = src[srcPos];
+            ++dstPos;
+            srcPos += dim;
+            dst[dstPos] = src[srcPos];
+            ++dstPos;
+            srcPos += dim;
+            dst[dstPos] = src[srcPos];
+            ++dstPos;
+            srcPos += dim;
+            dst[dstPos] = src[srcPos];
+            ++dstPos;
+            srcPos += dim;
+            dst[dstPos] = src[srcPos];
+            ++dstPos;
+            srcPos += dim;
+            dst[dstPos] = src[srcPos];
+            ++dstPos;
+            srcPos += dim;
+            dst[dstPos] = src[srcPos];
+            ++dstPos;
+            srcPos += dim;
+            dst[dstPos] = src[srcPos];
+        }
+    }
+}
+
+void rotate_32(int dim, pixel *src, pixel *dst) 
+{
+    int i, j;
+    int dstPos, srcPos;
+    int block=16;//blocking the Matrix
+    for(i=0; i<dim; i+=block)
+    {
+        for(j=0; j<dim; ++j)
+        {
+            dstPos = (dim-1-j)*dim+i;
+            srcPos = i*dim+j;
             dst[dstPos] = src[srcPos];
             ++dstPos;
             srcPos += dim;
@@ -162,7 +220,8 @@ void rotate(int dim, pixel *src, pixel *dst)
 void register_rotate_functions() 
 {
     add_rotate_function(&naive_rotate, naive_rotate_descr);   
-    add_rotate_function(&rotate, rotate_descr);   
+    add_rotate_function(&rotate_16, rotate_descr); 
+    add_rotate_function(&rotate_32, rotate_descr);  
     /* ... Register additional test functions here */
 }
 
